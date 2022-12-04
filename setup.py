4280752dbd5c4cfea8a0faba1155ed0382b94356
Dhrutiv Bhavsar <119542448+DHRVIR@users.noreply.github.com>
2022-12-04 13:09:46 +0100

    finalized setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 import os
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
+with open('requirements.txt') as files:
+    important = files.read().splitlines()
 setup( 
     name = "snowflake",
     author = "Dhrutiv Jasmin Bhavsar",
     version = "0.0.1",
+    url = "https://github.com/DHRVIR/Winter_is_coming.git",
+    author_email="bhavsardhrutiv@gmail.com",
     packages = find_packages(),
-    install_requires = required
+    install_requires = important
 )
