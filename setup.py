0fd472ea2f28721ea27256f15dfffb6f38c7195d
Dhrutiv Bhavsar <119542448+DHRVIR@users.noreply.github.com>
2022-12-04 12:19:29 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, packages
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as file:
     important = file.read().splitlines()
@@ -8,6 +8,6 @@ setup(
     version = "0.0.1",
     author_email = "bhavsardhrutiv@gmail.com",
     url = "https://github.com/DHRVIR/Winter_is_coming.git",
-    packages = packages(),
+    packages = find_packages(),
     install_requires = important,
 )
